bd906cc014c045e8e35ab5bb2c7cbebc28979ee8
RuslanMinyashev <72917421+RuslanMinyashev@users.noreply.github.com>
2020-10-29 15:37:16 +0400

    Add files via upload
    Задание №4 , не получилось реализовать проверку с помощью циклов выдает ошибку класса, никак не разберусь.

---
+++ mainApp.java
@@ -0,0 +1,150 @@
+package ru.lesson1;
+import java.util.Scanner;
+import java.util.Random;
+public class mainApp {
+    public static int SIZE = 3;
+    public static int DOTS_TO_WIN = 3;
+    public static final char DOT_EMPTY = '*';
+    public static final char DOT_X = 'X';
+    public static final char DOT_0 = '0';
+    public static char[][] map;
+    public static Scanner sc = new Scanner(System.in);
+    public static Random rand = new Random();
+
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Победил человек!");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья!");
+                break;
+            }
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_0)) {
+                System.out.println("Победил исскуственный интелект!");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья!");
+                break;
+            }
+        }
+        System.out.println("Игра закончена!");
+
+    }
+
+   /* public static boolean checkWin(char symb) {
+        if (map[0][0] == symb && map[0][1] == symb && map[0][2] == symb)
+            return true;
+        if (map[1][0] == symb && map[1][1] == symb && map[1][2] == symb)
+            return true;
+        if (map[2][0] == symb && map[2][1] == symb && map[2][2] == symb)
+            return true;
+        if (map[0][0] == symb && map[1][0] == symb && map[2][0] == symb)
+            return true;
+        if (map[0][1] == symb && map[1][1] == symb && map[2][1] == symb)
+            return true;
+        if (map[0][2] == symb && map[1][2] == symb && map[2][2] == symb)
+            return true;
+        if (map[0][0] == symb && map[1][1] == symb && map[2][2] == symb)
+            return true;
+        if (map[2][0] == symb && map[1][1] == symb && map[0][2] == symb)
+            return true;
+        return false;
+
+*/
+
+   boolean checkWin(char dot) {
+       for (int i = 0; i < SIZE; i++)
+           if ((map[i][0] == dot && map[i][1] == dot &&
+                   map[i][2] == dot) ||
+                   (map[0][i] == dot && map[1][i] == dot &&
+                           map[2][i] == dot))
+               return true;
+       if ((map[0][0] == dot && map[1][1] == dot &&
+               map[2][2] == dot) ||
+               (map[2][0] == dot && map[1][1] == dot &&
+                       map[0][2] == dot))
+           return true;
+       return false;
+   }
+    boolean isMapFull() {
+        for (int row = 0; row < SIZE; row++)
+            for (int col = 0; col < SIZE; col++)
+                if (map[row][col] == DOT_EMPTY)
+                    return false;
+        return true;
+    }
+    }
+
+    public static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static void aiTurn() {
+        int x, y;
+        do {
+            x = rand.nextInt(SIZE);
+            y = rand.nextInt(SIZE);
+        }
+        while (!isCellValid(x, y));
+        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
+        map[y][x] = DOT_0;
+    }
+
+    public static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты в формате X  Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        }
+        while (!isCellValid(x, y));
+        map[y][x] = DOT_X;
+    }
+
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE)
+            return false;
+        if (map[y][x] == DOT_EMPTY)
+            return true;
+        return false;
+    }
+
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+    public static void printMap() {
+        for (int i = 0; i < SIZE; i++) {
+            System.out.println(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
